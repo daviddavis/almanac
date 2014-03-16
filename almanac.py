326dd0a727178a0ed8b97b904814b66f1c2cd3c8
David Davis <daviddavis@redhat.com>
2014-03-16 17:44:13 -0400

    Added authors page

--- almanac.py
+++ almanac.py
@@ -11,5 +11,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/authors")
+def authors():
+    return render_template("authors.html")
+
 if __name__ == "__main__":
     app.run(debug = "True")
