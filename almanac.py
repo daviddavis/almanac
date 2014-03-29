4f3724e6c735f827649cc435fbd64d6c5fa8f243
David Davis <daviddavis@redhat.com>
2014-03-29 11:24:50 -0400

    Created a contest page

--- almanac.py
+++ almanac.py
@@ -1,9 +1,8 @@
-import os
 from flask import Flask
 from flask import render_template
-from flask import request
 
 app = Flask(__name__)
+
 
 #Create our index or root / route
 @app.route("/")
@@ -11,17 +10,26 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+
 @app.route("/authors")
 def authors():
     return render_template("authors.html")
+
 
 @app.route("/disclaimer")
 def disclaimer():
     return render_template("disclaimer.html")
 
+
 @app.route("/contents")
 def contents():
     return render_template("contents.html")
 
+
+@app.route("/contest/rules")
+def contest_rules():
+    return render_template("contest/rules.html")
+
+
 if __name__ == "__main__":
-    app.run(debug = "True")
+    app.run(debug="True")
